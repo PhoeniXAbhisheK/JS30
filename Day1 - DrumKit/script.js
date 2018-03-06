c904c19531fee12d7ece97e2156f8e22dd4032dd
PhoeniXAbhisheK <abhiy3010@gmail.com>
2018-03-06 13:04:29 +0530

    playAudio function seperated

--- Day1 - DrumKit/script.js
+++ Day1 - DrumKit/script.js
@@ -1,4 +1,4 @@
-window.addEventListener('keydown', function(e){
+function playAudio(e){
 	// console.log(e.keyCode);
 	const audioSrc = document.querySelector('audio[data-key="'+e.keyCode+'"]');
 	const keySrc = document.querySelector('.key[data-key="'+e.keyCode+'"]');
@@ -7,7 +7,7 @@ window.addEventListener('keydown', function(e){
 	audioSrc.currentTime = 0;
 	if(!audioSrc) return;
 	audioSrc.play();
-});
+}
 
 function removePlayingClass(e){
 	if(e.propertyName !== 'transform') return;
@@ -16,3 +16,5 @@ function removePlayingClass(e){
 
 const keys =  document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend',removePlayingClass));
+
+window.addEventListener('keydown', playAudio);
