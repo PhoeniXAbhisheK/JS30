ecdf769fca76d83f03ee6e2378c9d1ad2443c487
PhoeniXAbhisheK <abhiy3010@gmail.com>
2018-03-06 12:18:57 +0530

    Day 1 - of 30 for #JS30

---
+++ Day1 - DrumKit/script.js
@@ -0,0 +1,19 @@
+window.addEventListener('keydown', function(e){
+	// console.log(e.keyCode);
+	const audioSrc = document.querySelector('audio[data-key="'+e.keyCode+'"]');
+	const keySrc = document.querySelector('.key[data-key="'+e.keyCode+'"]');
+	keySrc.classList.add('playing');
+	// console.log(audioSrc);
+	audioSrc.currentTime = 0;
+	if(!audioSrc) return;
+	audioSrc.play();
+	// keySrc.classList.remove("playing");
+});
+
+function removePlayingClass(e){
+	if(e.propertyName !== 'transform') return;
+	this.classList.remove('playing');
+}
+
+const keys =  document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend',removePlayingClass));
