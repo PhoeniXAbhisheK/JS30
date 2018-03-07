282021faf13b2c09bf8d5dbd6efc8bcdc6028d48
PhoeniXAbhisheK <abhiy3010@gmail.com>
2018-03-07 12:09:36 +0530

    added click event(for use on Mobile)

--- Day1 - DrumKit/script.js
+++ Day1 - DrumKit/script.js
@@ -8,6 +8,16 @@ function playAudio(e){
 	if(!audioSrc) return;
 	audioSrc.play();
 }
+function playAudioClick(){
+	// console.log(this.getAttribute('data-key'));
+	const audioSrcClk = document.querySelector('audio[data-key="'+this.getAttribute('data-key')+'"]');
+	const keySrcClk = document.querySelector('.key[data-key="'+this.getAttribute('data-key')+'"]');
+	keySrcClk.classList.add('playing');
+	// console.log(audioSrc);
+	audioSrcClk.currentTime = 0;
+	if(!audioSrcClk) return;
+	audioSrcClk.play();
+}
 
 function removePlayingClass(e){
 	if(e.propertyName !== 'transform') return;
@@ -16,5 +26,6 @@ function removePlayingClass(e){
 
 const keys =  document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend',removePlayingClass));
+keys.forEach(key => key.addEventListener('click', playAudioClick));
 
 window.addEventListener('keydown', playAudio);
